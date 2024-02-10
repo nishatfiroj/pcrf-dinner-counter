fca13c7f3b2636b984580b76382955bb0226b272
Nishat Firoj <nishat.firoj@gmail.com>
2024-02-10 11:57:27 -0500

    update some of the credentials

--- app/gsheets.ts
+++ app/gsheets.ts
@@ -5,8 +5,10 @@ export async function getGoogleSheetsData(range: string) {
     projectId: "pcrf-dinner",
     credentials: {
       type: "service_account",
+      project_id: "pcrf-dinner",
+      private_key_id: process.env.PRIVATE_KEY_ID,
       private_key: process.env.PRIVATE_KEY,
-      client_email: process.env.CLIENT_SERVICE_ACCOUNT,
+      client_email: process.env.CLIENT_EMAIL,
       client_id: process.env.CLIENT_ID,
       token_url: "https://oauth2.googleapis.com/token",
       universe_domain: "googleapis.com",
--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,9 @@ const nextConfig = {
     CLIENT_ID: process.env.CLIENT_ID,
     API_KEY: process.env.API_KEY,
     SHEET_ID: process.env.SHEET_ID,
-    CLIENT_SERVICE_ACCOUNT: process.env.CLIENT_SERVICE_ACCOUNT,
+    CLIENT_EMAIL: process.env.CLIENT_EMAIL,
     PRIVATE_KEY: process.env.PRIVATE_KEY,
+    PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
   },
 }
 
